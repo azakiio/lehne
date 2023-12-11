9841081793463012d72c15ba90943244ce8da1cc
Adham <32067614+azakiio@users.noreply.github.com>
2023-12-11 18:42:23 +0100

    test adapter and antui

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,31 +6,25 @@ import rehypeExternalLinks from "rehype-external-links";
 import remarkGfm from "remark-gfm";
 import remarkSmartypants from "remark-smartypants";
 import UnoCSS from "unocss/astro";
+import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://astro-blog-template.netlify.app",
-  integrations: [
-    mdx(),
-    svelte(),
-    alpine(),
-    UnoCSS({
-      injectReset: true, // or a path to the reset file
-    }),
-  ],
+  integrations: [mdx(), svelte(), alpine(), UnoCSS({
+    injectReset: true // or a path to the reset file
+  }), react()],
   markdown: {
     shikiConfig: {
-      theme: "nord",
+      theme: "nord"
     },
     remarkPlugins: [remarkGfm, remarkSmartypants],
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          target: "_blank",
-        },
-      ],
-    ],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: "_blank"
+    }]]
   },
+  adapter: vercel()
 });
---
+++ src/components/DropDown.tsx
@@ -0,0 +1,64 @@
+import type { MenuProps } from "antd";
+import { Dropdown, Space } from "antd";
+
+const DD = () => {
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          1st menu item
+        </a>
+      ),
+    },
+    {
+      key: "2",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com"
+        >
+          2nd menu item (disabled)
+        </a>
+      ),
+      icon: <div />,
+      disabled: true,
+    },
+    {
+      key: "3",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.luohanacademy.com"
+        >
+          3rd menu item (disabled)
+        </a>
+      ),
+      disabled: true,
+    },
+    {
+      key: "4",
+      danger: true,
+      label: "a danger item",
+    },
+  ];
+
+  return (
+    <Dropdown menu={{ items }}>
+      <a onClick={(e) => e.preventDefault()}>
+        <Space>
+          Hover me
+          <div>{"->"}</div>
+        </Space>
+      </a>
+    </Dropdown>
+  );
+};
+
+export default DD;
