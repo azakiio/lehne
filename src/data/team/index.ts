25f558849ebf9f89f8fe33de9d0f139c8d881abf
admin-Lehne <156178684+admin-Lehne@users.noreply.github.com>
2024-01-12 13:44:04 +0100

    Update Bilder

--- src/data/team/index.ts
+++ src/data/team/index.ts
@@ -3,19 +3,16 @@ import MHimmelreich from "./MHimmelreich.jpg";
 import CLips from "./CLips.jpg";
 import OPaul from "./OPaul.jpg";
 import ETimmermann from "./ETimmermann.jpg";
-import NSchieke from "./NSchieke.jpg";
 import FHaensch from "./FHaensch.jpg";
 import VKolaska from "./VKolaska.jpg";
 import BRoemmer from "./BRoemmer.jpg";
 import NGoldberg from "./NGoldberg.jpg";
-import CGrittner from "./CGrittner.jpg";
 import EKaemper from "./EKaemper.jpg";
 import LBentin from "./LBentin.jpg";
 import LSchneider from "./LSchneider.jpg";
 import SMannes from "./SMannes.jpg";
 import TArmborst from "./TArmborst.jpg";
 import SLehmann from "./SLehmann.jpg";
-import NBrunken from "./NBrunken.jpg";
 import JSchmidt from "./JSchmidt.jpg";
 import HAldag from "./HAldag.jpg";
 import GNiemann from "./GNiemann.jpg";
@@ -25,33 +22,30 @@ import GNiemann from "./GNiemann.jpg";
 // import HAlsaoud from "./HAlsaoud.jpg";
 // import JWollatz from "./JWollatz.jpg";
 // import TOellrich from "./TOellrich.jpg";
-
+// import APulido from "./APulido.jpg";
 export const people = [
   { name: "S. Eikenroth", image: SEikenroth },
   { name: "M. Himmelreich", image: MHimmelreich },
   { name: "C. Lips", image: CLips },
   { name: "O. Paul", image: OPaul },
   { name: "E. Timmermann", image: ETimmermann },
-  { name: "N. Schieke", image: NSchieke },
   { name: "F. Hänsch", image: FHaensch },
   { name: "V. Kolaska", image: VKolaska },
   { name: "B. Römmer", image: BRoemmer },
   { name: "N. Goldberg", image: NGoldberg },
-  { name: "C. Grittner", image: CGrittner },
   { name: "E. Kämper", image: EKaemper },
   { name: "L. Bentin", image: LBentin },
   { name: "L. Schneider", image: LSchneider },
   { name: "S. Mannes", image: SMannes },
   { name: "T. Armborst", image: TArmborst },
   { name: "S. Lehmann", image: SLehmann },
-  { name: "N. Brunken", image: NBrunken },
   { name: "J. Schmidt", image: JSchmidt },
   { name: "H. Aldag", image: HAldag },
   { name: "G. Niemann-Hiessl", image: GNiemann },
-  { name: "F. Kohl", image: undefined },
   { name: "V. Saini", image: undefined },
   { name: "K. Baginski", image: undefined },
   { name: "H. Alsaoud", image: undefined },
   { name: "J. Wollatz", image: undefined },
   { name: "T. Oellrich", image: undefined },
+  { name: "A. Pulido", image: undefined },
 ];
