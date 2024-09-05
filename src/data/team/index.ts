ff4e72f3e27bd86fcedf7b4b218d61ef6055e637
admin-Lehne <156178684+admin-Lehne@users.noreply.github.com>
2024-09-05 15:10:12 +0200

    Update Lehne Teammitlieder

--- src/data/team/index.ts
+++ src/data/team/index.ts
@@ -23,10 +23,10 @@ import GLochte from "./GLochte2.jpg";
 import SLehmann from "./SLehmann.jpg";
 import SAlsalhi from "./SAlsalhi2.jpg";
 import HAldag from "./HAldag.jpg";
+//import MBiskup from "./MBiskup.jpg";
 //import LPetznik from "./LPetznik.jpg";
 //import HAlsaoud from "./HAlsaoud.jpg";
 //import NZiemann from "./Nziemann.jpg";
-//import NZiemann from "./LGrymlas.jpg";
 
 export const people = [
   { name: "S. Eikenroth", image: SEikenroth },
@@ -54,9 +54,8 @@ export const people = [
   { name: "T. Oellrich", image: TOellrich },
   { name: "S. Lehmann", image: SLehmann },
   { name: "H. Aldag", image: HAldag },
-  { name: "H. Alsaoud", image: undefined },
+  { name: "M. Biskup", image: undefined },
   { name: "L. Petznik", image: undefined },
-  { name: "J. Henke", image: undefined }, 
   { name: "M. Wettstaedt", image: undefined }, 
   { name: "N. Ziemann", image: undefined }, 
   { name: "L. Grymlas", image: undefined }, 
