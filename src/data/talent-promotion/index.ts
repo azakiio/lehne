61ee4e46591dee303f93cd668243698de7e3427f
admin-Lehne <156178684+admin-Lehne@users.noreply.github.com>
2025-09-26 11:17:19 +0200

    Bugfix #3 GWE Klein- Großschreibung

--- src/data/talent-promotion/index.ts
+++ src/data/talent-promotion/index.ts
@@ -7,8 +7,8 @@ import sailing2 from "./sailing/slide2.jpg";
 import sailing3 from "./sailing/slide3.jpg";
 import sailing4 from "./sailing/slide4.jpg";
 
-import gwe1 from "./gwe/00_Team_1.jpg"
+import GWE1 from "./GWE/00_Team_1.jpg"
 
 export const football = [football1, football2, football3];
 export const sailing = [sailing1, sailing2, sailing3, sailing4];
-export const gwe = [gwe1];
+export const GWE = [GWE1];
