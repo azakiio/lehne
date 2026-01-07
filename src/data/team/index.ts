12fb8644602102a7a02c46da961f05cccbc6fb5a
admin-Lehne <156178684+admin-Lehne@users.noreply.github.com>
2026-01-07 11:50:03 +0100

    Update Team

--- src/data/team/index.ts
+++ src/data/team/index.ts
@@ -2,25 +2,21 @@ import SEikenroth from "./SEikenroth.jpg";
 import ETimmermann from "./ETimmermann.jpg";
 import VKolaska from "./VKolaska.jpg";
 import BRoemmer from "./BRoemmer.jpg";
-import NGoldberg from "./NGoldberg.jpg";
 import EKaemper from "./EKaemper.jpg";
 import SMannes from "./SMannes.jpg";
-import TArmborst from "./TArmborst.jpg";
 import APulido from "./APulido.jpg";
 import JSchmidt from "./JSchmidt.jpg";
 import NHettche from "./NHettche2.jpg";
 import GNiemann from "./GNiemann.jpg";
-import JUhlenkamp from "./JUhlenkamp2.jpg";
 import TOellrich from "./TOellrich2.jpg";
 import SAlsalhi from "./SAlsalhi2.jpg";
 import JJoritz from "./JJoritz.jpg";
 import FHäußler from "./FHäußler.jpg";
-import VDürr from "./VDürr.jpg";
 import SVoßschulte from "./SVoßschulte.jpg";
+//import GZinßer from "./GZinßer.jpg";
 //import DKieburg from "./DKieburg.jpg";
 //import LPetznik from "./LPetznik.jpg";
 //import HNowak from "./HNowak.jpg";
-//import EBatholome from "./EBartholome.jpg";
 //import FLorenz from "./FLorenz.jpg";
 //import LNibbe from "./LNibbe.jpg";
 //import JKüsel from "./JKüsel.jpg";
@@ -42,11 +38,10 @@ export const people = [
   { name: "T. Oellrich", image: TOellrich },
   { name: "A. Pulido", image: APulido },
   { name: "J. Joritz", image: JJoritz },
-  { name: "T. Armborst", image: TArmborst },
   { name: "F. Häußler", image: FHäußler },
   { name: "S. Voßschulte", image: SVoßschulte },
-  { name: "V. Dürr", image: VDürr }, 
-  { name: "H. Nowak", image: undefined },
+  { name: "H. Nowak", image: undefined }, 
+  { name: "G. Zinßer", image: undefined },
   { name: "H. Repschläger", image: undefined },
   { name: "J. Herrmann", image: undefined },
   { name: "F. Lorenz", image: undefined },
@@ -58,4 +53,3 @@ export const people = [
   { name: "F. Schulz", image: undefined },
   
 
-];
